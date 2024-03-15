a41876fc48b9183ac2c5c9bf59f1c339b7fc3e90
Maria <mariia.harbuzenko.pm.2022@lpnu.ua>
2024-03-15 21:00:28 +0200

    login new

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', loadComponent: ()=> import('./Eatsy/login/login.component').then(a=>a.LoginComponent)},
+  {path: 'registration', loadComponent: ()=> import('./Eatsy/registration/registration.component').then(a=>a.RegistrationComponent)},
+  {path: 'forgot-password', loadComponent: ()=> import('./Eatsy/forgot-password/forgot-password.component').then(a=>a.ForgotPasswordComponent)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
