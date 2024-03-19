88e4c041fa66df0dc146f3802465ed147850604f
Maria <mariia.harbuzenko.pm.2022@lpnu.ua>
2024-03-19 16:43:42 +0200

    login and home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RegistrationComponent } from './Eatsy/registration/registration.component';
 import {NgOptimizedImage} from "@angular/common";
 import { ForgotPasswordComponent } from './Eatsy/forgot-password/forgot-password.component';
 import { LoginComponent } from './Eatsy/login/login.component';
 import {ReactiveFormsModule} from "@angular/forms";
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ForgotPasswordComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgOptimizedImage,
-    RegistrationComponent,
     ReactiveFormsModule,
     LoginComponent
   ],
